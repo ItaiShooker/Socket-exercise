e8d6f536c62c70faff178ade7bca4cbb80e6ae27
itayshooker <itaimeir.shooker@live.biu.ac.il>
2026-05-04 19:12:30 +0300

    upload the task

--- 1_udp_c.py
+++ 1_udp_c.py
@@ -1,14 +1,23 @@
+# Import the socket package
 from socket import socket, AF_INET, SOCK_DGRAM
 
+# define s as a socket
 s = socket(AF_INET, SOCK_DGRAM)
 
+# define the IP as "me"
 dst_ip = '127.0.0.1'
+# define the server IP
 dst_port = 12345
 
+# Send a request from me to the server
 s.sendto(b'Hello', (dst_ip,dst_port))
 
+# define the data and the details as what we get from the server 
 data, sender_info = s.recvfrom(2048)
+# Convert to string and print
 print(data.decode('utf-8'))
+# print the details
 print(sender_info)
 
+# close the socket
 s.close()
--- 3_tcp_s.py
+++ 3_tcp_s.py
@@ -1,19 +1,36 @@
+# import the socket package
 import socket
 
+# define server as a socket by tcp 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# get every client
 server_ip = ''
+# define the port
 server_port = 12345
+# connect between src and dest
 server.bind((server_ip, server_port))
+# wait only for 5 details
 server.listen(5)
 
+# infinity loop
 while True:
+    # get the client details 
     client_socket, client_address = server.accept()
     print('Connection from: ', client_address)
+    # define data as the client msg
     data = client_socket.recv(1024)
+    # while the msg is not finish
     while not data.decode('utf-8') == '':
+        # print the string
         print('Received: ', data.decode('utf-8'))
-        client_socket.send(data.upper())
+        # Convert to upper case
+        client_socket.send(data.lower())
+        # get the next
         data = client_socket.recv(1024)
 
+
     print('Client disconnected')
+    # close the socket
     client_socket.close()
+
+     # I made a chenge and now it returns a lower case
